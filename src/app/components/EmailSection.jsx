533611b925bdcdf1149a813cceb9cfb031ccd5f1
oosinloye <oosinloye@wisc.edu>
2023-12-12 02:58:39 -0600

    saved changed

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -32,8 +32,9 @@ const EmailSection = () => {
 
     const response = await fetch(endpoint, options);
     const resData = await response.json();
+    console.log(resData);
 
-    if (resData.status === 200) {
+    if (response.status === 200) {
       console.log("Message sent.");
       setEmailSubmitted(true);
     }
